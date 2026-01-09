e79b1c166e3b013ccdebf610d6f351a4ca71e457
Vescariu Jessica <jvescariu@gmail.com>
2026-01-09 12:09:21 +0200

    Initial Setup mobile project

---
+++ FitnessApp/FitnessMobile/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace FitnessMobile
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
